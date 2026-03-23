b53d5729b92d0da5f5f8d37b94cd2404a015c061
Raid225 <95387935+raid2256@users.noreply.github.com>
2026-03-23 11:56:13 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const REPO_OWNER = "raid2256";
 const REPO_NAME = "Hive-Progress-Tracker-Website";
+const WORKFLOW_FILE = "fetch-user.yml";
 
 // XP tables
 const LEVEL_TABLES = {
@@ -45,11 +46,10 @@ function calculateLevel(mode, xp) {
 }
 
 async function triggerWorkflow(username) {
-  await fetch(`https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/workflows/fetch-user.yml/dispatches`, {
+  await fetch(`https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/workflows/${WORKFLOW_FILE}/dispatches`, {
     method: "POST",
     headers: {
-      "Accept": "application/vnd.github+json",
-      "Authorization": `Bearer ${GITHUB_TOKEN}`
+      "Accept": "application/vnd.github+json"
     },
     body: JSON.stringify({
       ref: "main",
